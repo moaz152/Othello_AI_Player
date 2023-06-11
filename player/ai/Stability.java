68f0691ae2756fcd38498ad0c1bc9c0d69dfbb3d
Abdelrahman Atef Mohamed <43675149+hemany1@users.noreply.github.com>
2023-06-12 01:32:35 +0300

    Adding files
    Added the stability heuristic Ai agent

---
+++ player/ai/Stability.java
@@ -0,0 +1,42 @@
+package player.ai;
+
+import game.BoardHelper;
+
+import static player.ai.StaticEvaluator.*;
+
+public class Stability implements Evaluator {
+
+    //Evaluation Function Changes during Early-Game / Mid-Game / Late-Game
+    enum GamePhase {
+        EARLY_GAME,
+        MID_GAME,
+        LATE_GAME
+    }
+
+    private GamePhase getGamePhase(int[][] board){
+        int sc = BoardHelper.getTotalStoneCount(board);
+        if(sc<20) return GamePhase.EARLY_GAME;
+        else if(sc<=58) return GamePhase.MID_GAME;
+        else return GamePhase.LATE_GAME;
+    }
+
+    public int eval(int[][] board , int player){
+
+        //terminal
+        if(BoardHelper.isGameFinished(board)){
+            return 1000*evalDiscDiff(board, player);
+        }
+
+        //semi-terminal
+        switch (getGamePhase(board)){
+            case EARLY_GAME:
+                return evalDiscDiff(board,player);
+            case MID_GAME:
+                return evalDiscDiff(board,player);
+            case LATE_GAME:
+            default:
+                return evalDiscDiff(board,player);
+        }
+    }
+
+}
